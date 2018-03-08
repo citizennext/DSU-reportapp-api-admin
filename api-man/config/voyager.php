30f43986533e1afa92a9e81a14725208c361b143
Tudor Rusu <tudor.rusu@hotmail.com>
2018-03-08 20:21:04 +0100

    WiP

--- api-man/config/voyager.php
+++ api-man/config/voyager.php
@@ -168,7 +168,6 @@ return [
         'widgets' => [
             'TCG\\Voyager\\Widgets\\UserDimmer',
             'TCG\\Voyager\\Widgets\\PostDimmer',
-            'TCG\\Voyager\\Widgets\\PageDimmer',
         ],
 
     ],
